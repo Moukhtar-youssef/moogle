7c0432f5854d8b40ed9fb33153baa5ebbd21a1f3
Ionel Pop <ionelalexandru.pop@gmail.com>
2025-03-16 18:08:57 +0100

    Update spider main.go

--- services/spider/cmd/spider/main.go
+++ services/spider/cmd/spider/main.go
@@ -4,46 +4,66 @@ import (
     "log"
     "sync"
     "flag"
+    "os"
 
     "github.com/IonelPopJara/search-engine/services/spider/internal/pages"
-    "github.com/IonelPopJara/search-engine/services/spider/internal/links"
     "github.com/IonelPopJara/search-engine/services/spider/internal/crawler"
     "github.com/IonelPopJara/search-engine/services/spider/internal/database"
     "github.com/IonelPopJara/search-engine/services/spider/internal/controllers"
 )
 
+func getEnv(key, fallback string) string {
+    if value, exists := os.LookupEnv(key); exists {
+        return value
+    }
+
+    return fallback
+}
+
 func main() {
 
     // Parse flags
-    maxConcurrency := flag.Int("max-concurrency", 10, "Maximum number of concurrenet workers")
+    maxConcurrency := flag.Int("max-concurrency", 20, "Maximum number of concurrenet workers")
     maxPages := flag.Int("max-pages", 100, "Maximum number of pages per batch")
-    timeout := flag.Int("timeout", 5, "Maximum timeout allowed")
-    queueKey := flag.String("url-queue-name", "url_queue", "Key of the redis url queue")
 
     flag.Parse()
 
+    // Retrive environment variables
+    redisHost := getEnv("REDIS_HOST", "localhost")
+    redisPort := getEnv("REDIS_PORT", "6379")
+    redisPassword := getEnv("REDIS_PASSWORD", "")
+    redisDB := getEnv("REDIS_DB", "0")
+
     // Connect to Redis
     db := &database.Database{}
-    err := db.ConnectToRedis()
+    err := db.ConnectToRedis(redisHost, redisPort, redisPassword, redisDB)
     if err != nil {
-        log.Printf("Error: Couldn't connect to Redis: %v\n", err)
+        log.Printf("Error: %v\n", err)
+        return
+    }
+
+    // Add an entry to the message queue with score 0 (high priority)
+    err = db.PushURL("https://en.wikipedia.org/wiki/Kamen_Rider", 0)
+    if err != nil {
+        log.Printf("")
         return
     }
 
     // Instantiate controllers
     pageController := controllers.NewPageController(db)
-    outlinksController := controllers.NewOutlinksController(db)
+    linksController := controllers.NewLinksController(db)
+    imageController := controllers.NewImageController(db)
 
     // Instantiate crawler
     crawler := &crawler.CrawlerConfig {
         Mu:             &sync.Mutex{},
         Wg:             &sync.WaitGroup{},
         Pages:          make(map[string]*pages.Page),
-        Outlinks:       make(map[string]*links.Outlinks),
+        Outlinks:       make(map[string]*pages.PageNode),
+        Backlinks:      make(map[string]*pages.PageNode),
+        Images:         make(map[string][]*pages.Image),
         MaxPages:       *maxPages,
-        Timeout:        *timeout,
         MaxConcurrency: *maxConcurrency,
-        QueueKey:       *queueKey,
         CachedPages:    make(map[string]*pages.Page),
     }
 
@@ -65,14 +85,17 @@ func main() {
 
         // Write entries to the db
         pageController.SavePages(crawler)
-        outlinksController.SaveOutlinks(crawler)
-        log.Printf("DB updated!\n")
+        linksController.SaveLinks(crawler)
+        imageController.SaveImages(crawler)
 
         // Repopulate the CachedPages map to account for changes with other runners
         crawler.CachedPages = pageController.GetAllPages()
 
         // Clean visited pages by this runner
         crawler.Pages = make(map[string]*pages.Page)
+        crawler.Outlinks = make(map[string]*pages.PageNode)
+        crawler.Backlinks = make(map[string]*pages.PageNode)
+        crawler.Images = make(map[string][]*pages.Image)
     }
 }
 
