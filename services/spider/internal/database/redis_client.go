b2fa1d0ce25490e0a36ee91d882af643df8af009
Ionel Pop <ionelalexandru.pop@gmail.com>
2025-03-18 21:27:55 +0100

    Update spider
    Add printing for debugging

--- services/spider/internal/database/redis_client.go
+++ services/spider/internal/database/redis_client.go
@@ -17,6 +17,11 @@ type Database struct {
 }
 
 func (db *Database) ConnectToRedis(redisHost, redisPort, redisPassword, redisDB string) error {
+    log.Println("Connecting to Redis...")
+    log.Printf("\tRedis Host: '%s'\n", redisHost+":"+redisPort)
+    log.Printf("\tRedis Password: '%s'\n", redisPassword)
+    log.Printf("\tRedis DB: '%s'\n", redisDB)
+
     dbIndex, err := strconv.Atoi(redisDB)
     if err != nil {
         return fmt.Errorf("Could not parse DB value: %v\n", err)
