050fe545385161555cab37704a773478fb21f545
Ionel Pop <ionelalexandru.pop@gmail.com>
2025-04-24 00:05:43 +0200

    Use HTTPS

--- services/query-engine/app/Providers/AppServiceProvider.php
+++ services/query-engine/app/Providers/AppServiceProvider.php
@@ -20,18 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS globally
-        URL::forceScheme('https');
-
-        // Add security headers
-        $this->app['router']->middleware(function ($request, $next) {
-            $response = $next($request);
-
-            return $response->withHeaders([
-                'Strict-Transport-Security' => 'max-age=31536000; includeSubDomains',
-                'X-Frame-Options' => 'SAMEORIGIN',
-                'X-Content-Type-Options' => 'nosniff',
-            ]);
-        });
+        \URL::forceScheme('https');
     }
 }
