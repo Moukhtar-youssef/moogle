97d174f3cf04c7bfaf48d94b55e7d0106e2c4a8b
Ionel Pop <ionelalexandru.pop@gmail.com>
2025-04-29 02:01:35 +0200

    Update backlinks-processor service

--- services/backlinks-processor/data/redis_client.py
+++ services/backlinks-processor/data/redis_client.py
@@ -7,43 +7,48 @@ from models.backlinks import Backlinks
 
 # SETUP LOGGER
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 logger = logging.getLogger(__name__)
 
+
 class RedisClient:
-    def __init__(self, host='localhost', port=6379, password="", db=0, decode_responses=True):
+    def __init__(
+        self, host="localhost", port=6379, password="", db=0, decode_responses=True
+    ):
         try:
             self.client = redis.Redis(
                 host=host,
                 port=port,
                 password=password,
-                db = db,
-                decode_responses=decode_responses
+                db=db,
+                decode_responses=decode_responses,
             )
 
             self.client.ping()
-            logger.info('Successfully connected to redis!')
+            logger.info("Successfully connected to redis!")
         except Exception as e:
-            logger.error(f'Failed to connect to redis')
+            logger.error(f"Failed to connect to redis: {e}")
             self.client = None
 
     # --------------------- BACKLINKS ---------------------
     def get_all_backlinks_keys(self) -> Optional[List[str]]:
         if self.client is None:
-            logger.error(f'Redis connection not initialized')
+            logger.error(f"Redis connection not initialized")
             return
 
-        logger.info('Fetching all backlinks\' keys')
-        res = self.client.keys('backlinks:*')
+        logger.info("Fetching all backlinks' keys")
+        res = self.client.keys("backlinks:*")
         if res is None:
-            logger.info(f'No backlinks found')
+            logger.info(f"No backlinks found")
             return None
 
         return res
 
     def get_all_backlinks(self, backlinks: List[str]) -> Optional[List[Backlinks]]:
         if self.client is None:
-            logger.error(f'Redis connection not initialized')
+            logger.error(f"Redis connection not initialized")
             return None
 
         # Perform a batch operation
@@ -56,7 +61,7 @@ class RedisClient:
 
         res = pipeline.execute()
         if res is None:
-            logger.info('Pipeline execution was empty')
+            logger.info("Pipeline execution was empty")
             return None
 
         # Convert the results to Backlinks
@@ -69,7 +74,7 @@ class RedisClient:
 
     def remove_all_backlinks(self, backlinks: List[str]) -> Optional[int]:
         if self.client is None:
-            logger.error(f'Redis connection not initialized')
+            logger.error(f"Redis connection not initialized")
             return None
 
         pipeline = self.client.pipeline()
